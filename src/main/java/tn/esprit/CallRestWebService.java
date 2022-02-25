9a4ddd1ac6a1bd36ecb3bfd9a2be0605103a1bdd
aabdousouid <abdelwehedsouid@gmail.com>
2022-02-25 16:26:32 +0100

    ajout commentaire

--- src/main/java/tn/esprit/CallRestWebService.java
+++ src/main/java/tn/esprit/CallRestWebService.java
@@ -35,7 +35,7 @@ public class CallRestWebService {
 
 			//ip = responseObj.getString("origin");
 			ip = responseObj.getString("query");
-			System.out.println("ip : " + ip); 
+			System.out.println("ip Adresse: " + ip); 
 
 		} catch (IOException e) {
 			e.printStackTrace();
